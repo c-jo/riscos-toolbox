4cc4752cb3a952ca097e1eb96911f848db52fd65
Chris Johns <chris@lessthan3.org.uk>
2020-06-29 16:22:21 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="riscos_toolbox",
+    version="0.1.0",
+    author="Chris Johns",
+    author_email="chris@lessthan3.org.uk",
+    description="RISC OS Toolbox library",
+    long_description="Library to use the RISC OS Toolbox",
+    url="https://github.com/c-jo/riscos-toolbox",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: RISC OS",
+    ],
+    python_requires='>=3',
+)
