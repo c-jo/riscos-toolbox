91fccccbbcc3a52de06fe255b600baf82d44ac54
Chris Johns <chris@lessthan3.org.uk>
2021-09-28 08:56:28 +0100

    menu: start of support for entry manipulation

--- riscos_toolbox/objects/menu.py
+++ riscos_toolbox/objects/menu.py
@@ -1,9 +1,36 @@
 from . import Object
+import swi
 
 class Menu(Object):
     class_id = 0x828c0
+    class Entry:
+        def __init__(self, menu, id):
+            self.menu = menu
+            self.id   = id
+
+        @property
+        def tick(self):
+            return swi.swi('Toolbox_ObjectMiscOp','0II;I', self.menu.id, 1, self.id)
+
+
+        @tick.setter
+        def tick(self, val):
+            swi.swi('Toolbox_ObjectMiscOp','0IIII', self.menu.id, 0, self.id, 1 if val else 0)
+
+        @property
+        def fade(self):
+            return swi.swi('Toolbox_ObjectMiscOp','0II;I', self.menu.id, 3, self.id,)
+
+
+        @fade.setter
+        def fade(self, val):
+            swi.swi('Toolbox_ObjectMiscOp','0IIII', self.menu.id, 2, self.id, 1 if val else 0)
+
     def __init__(self, id):
         super().__init__(id)
+
+    def __getitem__(self, item):
+        return Menu.Entry(self, item)
 
     def event_handler(self, event_code, id_block, poll_block):
         if event_code == 0x828c3: # Menu_Selection
