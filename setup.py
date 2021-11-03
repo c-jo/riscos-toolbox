244eeda0c67e8b16b151dc3c01fe34d38e14b023
Chris Johns <chris@lessthan3.org.uk>
2021-11-03 15:31:55 +0000

    Release 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="riscos_toolbox",
-    version="0.1.0",
+    version="0.2.0",
     author="Chris Johns",
     author_email="chris@lessthan3.org.uk",
     description="RISC OS Toolbox library",
