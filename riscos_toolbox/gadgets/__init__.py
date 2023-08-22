c-jo/riscos-toolbox

commit d68597267df0731ad4350e976f7b9a856a88cfc3

Fix ScrollList get/set state (bug #9) (#11)

riscos_toolbox/gadgets/__init__.py
@@ -58,13 +58,13 @@ class Gadget:
 
     def _miscop_set_int(self, op, value):
         """Use Toolbox_ObjectMiscOp to set an unsigned integer."""
-        swi.swi('Toolbox_ObjectMiscOp', '0IIII',
-                self.window.id,op,self.id,value)
+        swi.swi('Toolbox_ObjectMiscOp', '0iIII',
+                self.window.id, op, self.id, value)
 
     def _miscop_get_int(self, op):
         """Use Toolbox_ObjectMiscOp to get an unsigned integer."""
-        return swi.swi('Toolbox_ObjectMiscOp', '0III;I',
-                       self.window.id,op,self.id)
+        return swi.swi('Toolbox_ObjectMiscOp', '0iII;I',
+                       self.window.id, op, self.id)
 
     def _miscop_set_text(self, op, text):
         """Use Toolbox_ObjectMiscOp to set a string."""
@@ -77,8 +77,8 @@ class Gadget:
         buf_size = swi.swi('Toolbox_ObjectMiscOp', '0III00;.....I',
                            self.window.id,op,self.id)
         buffer = swi.block((buf_size+3)//4)
-        swi.swi('Toolbox_ObjectMiscOp', '0IIIbi',
-                           self.window.id,op,self.id,buffer,buf_size)
+        swi.swi('Toolbox_ObjectMiscOp', '0iIIIbI',
+                           self.window.id, op,self.id, buffer, buf_size)
         return buffer.nullstring()
 
 class GadgetDefinition(ctypes.Structure):

riscos_toolbox/gadgets/scrolllist.py
@@ -9,12 +9,12 @@ class ScrollList(Gadget):
 
     @property
     def state(self):
-        return swi.swi("Toolbox_ObjectMiscOp","0II;I",
+        return swi.swi("Toolbox_ObjectMiscOp","0iIi;I",
                        self.window.id, 16410, self.id)
 
     @state.setter
     def state(self, state):
-        swi.swi("Toolbox_ObjectMiscOp","0III",
+        swi.swi("Toolbox_ObjectMiscOp","0iIiI",
                 self.window.id, 16411, self.id, state)
 
     def add_item(self, text, index):

commit f5e2946d0a80ce296394439ed38e2ff5528ed6c2

Fix breaking change in _miscop_get_text
The committed fix for issue #9 d685972 broke _miscop_get_text by adding an extra integer argument to the format string; this affects all gadgets with text attributed. This was also merged into main.
This commit removes the extra argument and also flips the component ID arg to a signed int.

riscos_toolbox/gadgets/__init__.py
@@ -77,7 +77,7 @@ class Gadget:
         buf_size = swi.swi('Toolbox_ObjectMiscOp', '0III00;.....I',
                            self.window.id,op,self.id)
         buffer = swi.block((buf_size+3)//4)
-        swi.swi('Toolbox_ObjectMiscOp', '0iIIIbI',
+        swi.swi('Toolbox_ObjectMiscOp', '0iIibI',
                            self.window.id, op,self.id, buffer, buf_size)
         return buffer.nullstring()
 